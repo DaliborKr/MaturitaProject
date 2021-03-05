63813e2d7ae9ac8c8898bfb1c10302ec129d9e26
Dalibor Kri <dalidalk@seznam.cz>
2021-03-05 08:51:25 +0100

    Graphics – background
    Added my own background (parallax not yet)

---
+++ MaturitaProject/Assets/Scripts/Background/BackgroundWithCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundWithCamera : MonoBehaviour
+{
+    private float startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+    public float xPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x);
+
+        transform.position = new Vector3(startpos + dist + xPos, cam.transform.position.y, transform.position.z);
+    }
+}
